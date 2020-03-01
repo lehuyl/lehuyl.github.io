e3644bdcecc6966f93d92e010ffb5c28257116d7
lehuyl <stevenle18@gmail.com>
2020-02-29 18:44:22 -0800

    Update Footer and Header
    Signed-off-by: lehuyl <stevenle18@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,8 @@ class Footer extends React.Component {
                 <div className="inner">
                     <ul className="icons">
                         <li><a href="https://github.com/lehuyl" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="https://www.linkedin.com/in/-stevenle/" className="icon fa-linkedin"><span className="label">Dribbble</span></a></li>
+                        <li><a href="https://www.linkedin.com/in/-stevenle/" className="icon fa-linkedin"><span className="label">Linkedin</span></a></li>
+                        <li><a href="https://drive.google.com/open?id=1nStsrwxQiz0DyXbnG5xZ4w_9fmREYmnm" className="icon fa-file"><span className="label">Resume</span></a></li>
                     </ul>
                 </div>
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 import Footer from './Footer'
 import avatar from '../assets/images/avatar.jpg'
+import profile from '../assets/images/profile.png'
+
 
 class Header extends React.Component {
     render() {
         return (
             <header id="header">
                 <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
+                    <a href="#" className="image avatar"><img src={profile} alt="Site Profile Picture" /></a>
                     <h1><strong>Steven Le</strong></h1>
                     <h1>Software Developer Intern</h1>
                     <h1>I like to gym and learn new things in my spare time.</h1>
